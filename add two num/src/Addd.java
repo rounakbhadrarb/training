0bb2512dcad5b1f3673282a58f76e6fd8b992317
rounakbhadrarb <88178356+rounakbhadrarb@users.noreply.github.com>
2021-10-27 15:34:31 +0530

    Add files via upload

---
+++ add two num/src/Addd.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Addd {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int i=sc.nextInt();
+		int j=sc.nextInt();
+		System.out.println("the sum is "+(i+j));
+
+	}
+
+}
